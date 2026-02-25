8a416fee67250b4adac65cffef9948afa4733b55
Boteina <boteinajbel@gmail.com>
2026-02-25 23:37:34 +0000

    save devs

--- CampusManager/src/org/mql/campusmanager/services/ProfessorService.java
+++ CampusManager/src/org/mql/campusmanager/services/ProfessorService.java
@@ -1,5 +1,39 @@
 package org.mql.campusmanager.services;
 
+import java.util.Vector;
+
+import org.mql.campusmanager.models.Professor;
+
 public class ProfessorService {
 
+	private Vector<Professor> professors;
+	
+	public ProfessorService() {
+		professors = new Vector<>();
+	}
+	
+	public void addProfessor(Professor professor) {
+		for(Professor p : professors) {
+			if(p.getMatricule().equals(professor.getMatricule())) {
+				System.out.println("Professor " + professor.getFullName() + "already exists");
+				return;
+			}
+		}
+		professors.add(professor);
+		System.out.println("Professor added successfully.");
+	}
+	
+	public void removeProfessor(Professor professor) {
+		for(int i = 0; i<professors.size(); i++) {
+			if(professors.get(i).getMatricule().equals(professor.getMatricule())) {
+				professors.remove(i);
+				System.out.println("Professor removed successfully.");
+                return;
+			}
+		}
+		System.out.println("Professor not found.");
+	}
+	
+	
+	
 }
