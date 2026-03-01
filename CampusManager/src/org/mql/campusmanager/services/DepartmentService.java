c910ac3d263d6646fbfe1ca7b1ed6b8f61c363b7
Boteina <boteinajbel@gmail.com>
2026-03-01 20:34:36 +0000

    save devs

--- CampusManager/src/org/mql/campusmanager/services/DepartmentService.java
+++ CampusManager/src/org/mql/campusmanager/services/DepartmentService.java
@@ -1,8 +1,25 @@
 package org.mql.campusmanager.services;
 
-public class DepartmentService {
-	public void ccc() {
-		
-	}
+import java.util.Vector;
 
+import org.mql.campusmanager.models.Department;
+
+public class DepartmentService {
+	
+	private Vector<Department> departments;
+	
+	public DepartmentService() {
+		departments = new Vector<>();
+	}
+	
+	public void addDepartment(Department department) {
+		for(Department d : departments) {
+			if(d.getName().equals(department.getName())) {
+				System.out.println("Department " + department.getName() + "already exists");
+				return;
+			}
+		}
+		departments.add(department);
+		System.out.println("Department added successfully.");
+	}
 }
