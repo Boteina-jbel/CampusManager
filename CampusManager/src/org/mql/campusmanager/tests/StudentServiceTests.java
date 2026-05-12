3fa0a58ee44094b1b221234f89699613d98ad09c
Boteina <boteinajbel@gmail.com>
2026-05-12 01:34:27 +0100

    adding Student Service Tests

--- CampusManager/src/org/mql/campusmanager/tests/StudentServiceTests.java
+++ CampusManager/src/org/mql/campusmanager/tests/StudentServiceTests.java
@@ -2,6 +2,8 @@ package org.mql.campusmanager.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Vector;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -46,4 +48,38 @@ class StudentServiceTests {
 		assertEquals(student.getCNE(), found.getCNE());
 	}
 
+	@Test
+	void testRemoveStudent() {
+		//Arrange
+		Student newStudent = new Student(
+		        "Sara",
+		        "Ali",
+		        "sara@gmail.com",
+		        "K222222222"
+		    );
+		service.addStudent(newStudent);
+		//Act
+		service.removeStudent(student);
+		//Assert
+		assertEquals(1, service.listAllStudents().size());
+	}
+	
+	@Test
+	void testlistAllStudent() {
+		//Arrange
+		 Student newStudent = new Student(
+			        "Sara",
+			        "Ali",
+			        "sara@gmail.com",
+			        "K222222222"
+			    );
+
+		 service.addStudent(newStudent);
+			    
+		//Act
+		Vector<Student> students = service.listAllStudents();
+		//Assert
+		assertNotNull(service);
+		assertEquals(2, students.size());
+	}
 }
