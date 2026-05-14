ca587ea75f80c8a0aea8a4b3091806227328d139
Boteina <boteinajbel@gmail.com>
2026-05-14 23:28:48 +0100

    save devs

---
+++ CampusManager/src/org/mql/campusmanager/tests/CourseServiceTests.java
@@ -0,0 +1,23 @@
+package org.mql.campusmanager.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CourseServiceTests {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testassignCourseSuccess() {
+		//Arrange
+		
+		//Act
+		
+		//Assert
+	}
+
+}
