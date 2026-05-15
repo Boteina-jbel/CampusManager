949c529a309800bf034d89772421a7e8c3a5ee51
Boteina <boteinajbel@gmail.com>
2026-05-15 12:55:59 +0100

    testing CourseService

--- CampusManager/src/org/mql/campusmanager/tests/CourseServiceTests.java
+++ CampusManager/src/org/mql/campusmanager/tests/CourseServiceTests.java
@@ -4,20 +4,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mql.campusmanager.models.Course;
+import org.mql.campusmanager.models.Professor;
+import org.mql.campusmanager.services.CourseService;
 
 class CourseServiceTests {
 
+	private CourseService service;
+	private Course course;
+	private Professor professor;
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		service = new CourseService();
+		professor = new Professor("Loqman", "Loqman", "loqman@gmail.com", "LJ2825");
+		course = new Course("J201",  "Java", 2, 30, professor);
 	}
 
 	@Test
-	void testassignCourseSuccess() {
+	void testassignCourseFailure() {
 		//Arrange
-		
+		professor.addCourse(course);
 		//Act
-		
+		boolean result = service.assignCourse(professor, course);
 		//Assert
+		assertFalse(result);
 	}
 
 }
