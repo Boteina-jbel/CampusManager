dc9840dd4aee27b6717099107ec05db9e7eb3883
Boteina <boteinajbel@gmail.com>
2026-09-19 23:48:56 +0100

    feat: complete professor service implementation

--- CampusManager/src/org/mql/campusmanager/services/ProfessorService.java
+++ CampusManager/src/org/mql/campusmanager/services/ProfessorService.java
@@ -3,48 +3,92 @@ package org.mql.campusmanager.services;
 import java.util.Vector;
 
 import org.mql.campusmanager.models.Professor;
+import org.mql.campusmanager.repositories.ProfessorRepository;
 
 public class ProfessorService {
 
-	private Vector<Professor> professors;
+	private ProfessorRepository repository;
 	
 	public ProfessorService() {
-		professors = new Vector<>();
+		repository = new ProfessorRepository();
 	}
 	
 	public boolean addProfessor(Professor professor) {
-		for(Professor p : professors) {
-			if(p.getMatricule().equals(professor.getMatricule())) {
-				return false;
-			}
+		if(professor == null) {
+			return false;
 		}
-		professors.add(professor);
-		return true;
+		
+		if(repository.findByMatricule(professor.getMatricule()) != null) {
+			return false;
+		}
+		
+		return repository.save(professor);
 	}
 	
 	public boolean removeProfessor(Professor professor) {
-		for(int i = 0; i<professors.size(); i++) {
-			if(professors.get(i).getMatricule().equals(professor.getMatricule())) {
-				professors.remove(i);
-                return true;
-			}
+		if(professor == null) {
+			return false;
 		}
-		return false;
+		
+		Professor existingProfessor = repository.findByMatricule(professor.getMatricule());
+		
+		if(existingProfessor == null) {
+			return false;
+		}
+		return repository.delete(existingProfessor);
 	}
 	
 	public Professor findProfessorByMatricule(String matricule) {
-		for(Professor p : professors) {
-			if(p.getMatricule().equals(matricule)) {
-				return p;
-			}
-		}
-		return null;
+		return repository.findByMatricule(matricule);
 	}
 	
 	public Vector<Professor> listAllProfessors(){
-		//return professors;
-		return new Vector<>(professors);
+		return repository.findAll();
 	} 
 	
-	
 }
+
+
+
+
+
+//
+//private Vector<Professor> professors;
+//
+//public ProfessorService() {
+//	professors = new Vector<>();
+//}
+//
+//public boolean addProfessor(Professor professor) {
+//	for(Professor p : professors) {
+//		if(p.getMatricule().equals(professor.getMatricule())) {
+//			return false;
+//		}
+//	}
+//	professors.add(professor);
+//	return true;
+//}
+//
+//public boolean removeProfessor(Professor professor) {
+//	for(int i = 0; i<professors.size(); i++) {
+//		if(professors.get(i).getMatricule().equals(professor.getMatricule())) {
+//			professors.remove(i);
+//            return true;
+//		}
+//	}
+//	return false;
+//}
+//
+//public Professor findProfessorByMatricule(String matricule) {
+//	for(Professor p : professors) {
+//		if(p.getMatricule().equals(matricule)) {
+//			return p;
+//		}
+//	}
+//	return null;
+//}
+//
+//public Vector<Professor> listAllProfessors(){
+//	//return professors;
+//	return new Vector<>(professors);
+//} 
