35a9459b5e061be2b08b9a10adf851c5461aaef9
Boteina <boteinajbel@gmail.com>
2026-09-20 13:33:24 +0000

    feat: Add CourseRepository operations

--- CampusManager/src/org/mql/campusmanager/repositories/CourseRepository.java
+++ CampusManager/src/org/mql/campusmanager/repositories/CourseRepository.java
@@ -1,5 +1,35 @@
 package org.mql.campusmanager.repositories;
 
+import java.util.Vector;
+
+import org.mql.campusmanager.models.Course;
+
 public class CourseRepository {
 
+	private Vector<Course> courses;
+	
+	public CourseRepository() {
+		courses = new Vector<>();
+	}
+	
+	public boolean save(Course course) {
+		return courses.add(course);
+	}
+	
+	public boolean delete(Course course) {
+		return courses.remove(course);
+	}
+	
+	public Course findByCode(String code) {
+		for(Course course : courses) {
+			if(course.getCode().equals(code)) {
+				return course;
+			}
+		}
+		return null;
+	}
+	
+	public Vector<Course> findAll(){
+		return new Vector<>(courses);
+	}
 }
