76b32588acd043dca948fcc94a54a53be1361a4d
Boteina <boteinajbel@gmail.com>
2026-09-20 14:02:13 +0000

    feat: implement CourseService operations

--- CampusManager/src/org/mql/campusmanager/services/CourseService.java
+++ CampusManager/src/org/mql/campusmanager/services/CourseService.java
@@ -1,22 +1,75 @@
 package org.mql.campusmanager.services;
 
+import java.util.Vector;
+
 import org.mql.campusmanager.models.Course;
 import org.mql.campusmanager.models.Professor;
+import org.mql.campusmanager.repositories.CourseRepository;
 
 public class CourseService {
 	
-	public boolean assignCourse(Professor professor, Course course) {
-		if(professor.getCourses().contains(course)) {
-            System.out.println("Professor " + professor.getFullName() + " already teaches " + course.getName());
+	private CourseRepository repository;
+	
+	public CourseService() {
+		repository = new CourseRepository();
+	}
+	
+	public boolean addCourse(Course course) {
+		if(course == null) {
 			return false;
 		}
 		
-		professor.addCourse(course);
-        // assign professor to course
-		course.setProfessor(professor);
+		if(repository.findByCode(course.getCode()) != null) {
+			return false;
+		}
 		
-        System.out.println("Course " + course.getName() + " assigned to Professor " + professor.getFullName());
-		return true;
+		return repository.save(course);
+	}
+	
+	public boolean removeCourse(Course course) {
+		if(course == null) {
+			return false;
+		}
+		
+	    Course existingCourse = repository.findByCode(course.getCode());
+	    
+	    if (existingCourse == null) {
+	    	return false;
+	    }
+		
+		return repository.delete(existingCourse);
+	}
+	
+	public Course findCourseByCode(String code) {
+		return repository.findByCode(code);
+	}
+	
+	public Vector<Course> listAllCourses(){
+		return repository.findAll();
+	}
+	
+	public boolean assignCourse(Professor professor, Course course) {
+
+	    if (professor == null || course == null) {
+	        return false;
+	    }
+
+	    if (professor.getCourses().contains(course)) {
+	        System.out.println("Professor " + professor.getFullName() + " already teaches " + course.getName());
+	        return false;
+	    }
+
+	    if (course.getProfessor() != null) {
+	        System.out.println("Course " + course.getName()+ " is already assigned to a professor.");
+	        return false;
+	    }
+
+	    professor.addCourse(course);
+	    course.setProfessor(professor);
+
+	    System.out.println("Course " + course.getName() + " assigned to Professor " + professor.getFullName());
+
+	    return true;
 	}
 
 }
